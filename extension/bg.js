20023c307c9c351f2751faa47bdfcaaaf9d3cc1a
chartreume <281644581@qq.com>
2019-08-14 10:28:42 +0800

    del
    del ex

--- extension/bg.js
+++
@@ -1,4 +0,0 @@
-//var myWorker = new SharedWorker("/js/worker.js");
-//myWorker.port.onmessage = function(e) {
-//  console.log(e);
-//}
--- extension/fd.js
+++
@@ -1,2 +0,0 @@
-//window.top.postMessage({a:"1"},"http://localhost:3000");
-//window.addEventListener('message',function(e){console.log(e)});
